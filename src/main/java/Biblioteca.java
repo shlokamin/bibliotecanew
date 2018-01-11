99afba70ae0e892a4419a34caa26c5b660bcf177
Jackie Robinson <jrobinso@thoughtworks.com>
2018-01-11 16:19:52 -0600

    Jackie - list books story card completed

--- src/main/java/Biblioteca.java
+++ src/main/java/Biblioteca.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.io.PrintStream;
+import java.util.ArrayList;
 
 public class Biblioteca {
+    private PrintStream printStream;
+    private ArrayList<String> books;
+
+    public Biblioteca(PrintStream printStream) {
+        this.printStream = printStream;
+        books = new ArrayList<>();
+        books.add("Harry Potter");
+        books.add("Lord of the Rings");
+    }
+
+    public void displayWelcomeMessage() {
+        printStream.println("Welcome to Biblioteca");
+    }
+
+    public void listBooks() {
+        //String allBooks;
+
+        for (String book : books) {
+            printStream.println(book);
+        }
+
+        //printStream.
+
+    }
 }
--- src/test/java/WelcomeMessageTests.java
+++ src/test/java/WelcomeMessageTests.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 public class WelcomeMessageTests {
+    private PrintStream printstream;
+    private Biblioteca biblioteca;
+
+    @Before
+    public void setUp()throws Exception{
+        printstream = mock(PrintStream.class);
+        biblioteca = new Biblioteca(printstream);
+    }
+
+    @Test
+    public void shouldDisplayWelcomeMessageWhenAppStarts(){
+        String message = "Welcome to Biblioteca";
+
+        biblioteca.displayWelcomeMessage();
+
+        verify(printstream).println("Welcome to Biblioteca");
+    }
+
+    @Test
+    public void shouldDisplayBooksAfterWelcomeMessage() {
+        biblioteca.listBooks();
+
+        verify(printstream).println("Harry Potter");
+        verify(printstream).println("Lord of the Rings");
+
+
+    }
+
+
 }
